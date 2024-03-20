9642ddee5dbd827a5650a0de5138595ab98febaf
Mihit19 <mrmihitkumar@gmail.com>
2024-03-20 23:35:53 +0530

    ..

--- lib/features/shop/screens/home.dart
+++ lib/features/shop/screens/home.dart
@@ -1,5 +1,3 @@
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:explore_ease/common/widgets/custom_shape/container/circular_container.dart';
 import 'package:explore_ease/features/shop/screens/widgets/home_appbar.dart';
 import 'package:explore_ease/features/shop/screens/widgets/home_categories.dart';
 import 'package:explore_ease/features/shop/screens/widgets/promo_slider.dart';
@@ -9,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import '../../../common/widgets/custom_shape/container/primary_header_container.dart';
 import '../../../common/widgets/custom_shape/container/search_container.dart';
-import '../../../common/widgets/images/EE_rounded_image.dart';
 import '../../../common/widgets/texts/section_heading.dart';
 
 class HomeScreen extends StatelessWidget {
